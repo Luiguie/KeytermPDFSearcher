4b5f70dcb51c839b01d40a236d10b7fda5df8b66
Luiguie <59609978+Luiguie@users.noreply.github.com>
2022-11-07 23:44:35 -0300

    Add files via upload

--- Exe version/KeyTerm_PDF_Searcher.py
+++ Exe version/KeyTerm_PDF_Searcher.py
@@ -1,7 +1,7 @@
 # import packages
-from email import message
 import PyPDF2
 import re
+import webbrowser
 import tkinter as tk
 from pathlib import Path
 from tkinter import *
@@ -9,85 +9,113 @@ from tkinter import filedialog
 from tkinter import ttk
 
 chosenFile = ""
+foundPDFList = []
+def openPDFs():
+    for pdf in foundPDFList:
+        webbrowser.open_new(pdf)
+    
+def MyGithub():
+    webbrowser.open("https://github.com/Luiguie", new=1)
+
 def BrowseFile():
+    tx.config(state=NORMAL)
+    tx.delete(1.0, "end")
     global chosenFile 
     chosenFile = filedialog.askdirectory()
-    #label_file_explorer.configure(text="Chosen File: " + chosenFile)
+    tx.insert("end", "Your Folder: \n" + chosenFile + "\n")
+    tx.config(state=DISABLED)
 
 def aa(cf):
+    tx.config(state=NORMAL)
+    tx.delete(1.0, "end")
+    tx.insert("end", "Your Folder: \n" + chosenFile + "\n")
     # returns all file paths that has .pdf as extension in the specified directory
-    print(cf)
     pdf_search = Path("%s" %(cf)).glob("*.pdf")
-    # convert the glob generator out put to list
-    # skip this if you are comfortable with generators and pathlib
+    # convert the glob generator output to list
     pdf_files = [str(file.absolute()) for file in pdf_search]
-    message = "Your files:", pdf_files
-    tx.insert('end', message)
-    #print("Seus arquivos sao:", pdf_files)
-    #define keyterms
-    #key_term = input("Please insert keyterm (Be specific)\n")
-    global eDir
-    key_term = eDir.get()
+    global KT
+    key_term = KT.get()
+    emptyPDF_Results = True
+    logs = ""
     for pdf in pdf_files:    
         # open the pdf file
         object = PyPDF2.PdfFileReader("%s" %(pdf))
-            
-        print(pdf)
         slen = len(pdf)
-        print(slen)
         for i in range((slen-1),0, -1):
             if pdf[i] == "\\":
-                message = "\n" + pdf[i:slen] +"\n"
-                tx.insert('end', message)
+                mInfo = "\n" + pdf[(i+1):slen] +"\n"
                 break
         # get number of pages
         NumPages = object.getNumPages()
-        tx.insert('end', message)
+        mPage = " "
+        
         # extract text and do the search
         for i in range(0, NumPages):
-            PageObj = object.getPage(i)
-            message = "Current Page: " + str(i+1) +"\n"
-            tx.insert('end', message)
-                #print("Current Page: " + str(i+1) ) 
+            PageObj = object.getPage(i)      
             pdfText = PageObj.extractText() 
+            pdfText = pdfText.lower()
+            key_term = key_term.lower()
             ResSearch = re.search(key_term, pdfText)
                 # Displays whether or not the keyterm was found
-            if ResSearch == None:
-                message = "Not Found\n"
-                tx.insert('end', message)                    
-            else:
-                message = "Keyterm Found\n"
-                tx.insert('end', message)
+            pdfCheck = False
+            if ResSearch != None:
+                pdfCheck = True
+                if i == 0:
+                    mPage += str(i+1)
+                elif i<(NumPages-1):
+                    mPage += ", " + str(i+1)
+                else:
+                    if mPage == " ":
+                        mPage += str(i+1)    
+                    else:
+                        mPage += " and " + str(i+1)  
+                        
+        if pdfCheck == True:
+            emptyPDF_Results = False
+            message = mInfo + "KeyTerm not found on page(s):" + mPage + "\n"
+            logs += message
+            tx.insert('end', message)
+            foundPDFList.append(pdf)
+            print(foundPDFList)
+            pdfOpen.config(state=NORMAL)
+        else:
+            logs += mInfo + "KeyTerm not found on PDF" + "\n"
+    if emptyPDF_Results == True:
+        tx.insert("end", "\nKeyTerm not found on chosen folder")
+        pdfOpen.config(state=DISABLED)
+    
+    tx.insert("end", "\n\n\n\n#######################\nLogs:\n" + logs)
+    tx.config(state=DISABLED)
+
+
 
 # window instance and config
 wd = Tk()
 wd.title("KeyTerm PDF Searcher by Luiguie")
 wd.geometry("800x450")
-tx = Text(wd, height=40, width=50)
-tx.pack(expand=True)
-tx.pack(padx=0, pady=0, side=tk.LEFT)
+#wd.resizable(0,0)
+bOpen = Button(wd, text = "Select Folder", command = BrowseFile)
+bOpen.grid(row=12,column=0, sticky="sw")
+
+KT = Entry(wd, bd=5)
+KT.grid(row=13,column=0, sticky="n")
+bSearch = Button(wd, text = "➢", command= lambda:aa(chosenFile))
+bSearch.grid(row=13,column=1, sticky="n")
+credits = Button(wd, text = "Made By Luiguie", command= MyGithub)
+credits.grid(row=20,column=0, sticky="sw")
+
+tx = Text(wd, height=25, width=48)
+tx.grid(row=2, column=2, padx=(200,0), rowspan=20, columnspan=10)
+tx.config(state=DISABLED)
 scrollbar = ttk.Scrollbar(
     wd,
     orient='vertical',
-    command=tx.yview
+    command=tx.yview, 
 )
-scrollbar.pack(padx=0, pady=0, side=tk.LEFT)
-bOpen = Button(wd, text = "Select Folder", command = BrowseFile)
-#bOpen.grid(column = 10, row = 20)
-bOpen.pack(padx=5, pady=200, side=tk.RIGHT)
-bSearch = Button(wd, text = "➢", command= lambda:aa(chosenFile))
-bSearch.pack(padx=5, pady=200, side=tk.RIGHT)
-eDir = Entry(wd, bd=5)
-eDir.pack(padx=5, pady=200, side=tk.RIGHT)
+scrollbar.grid(row=2, column=25, rowspan=20 ,sticky="ns")
+
+pdfOpen = Button(wd, text = "Open Found PDFs", command= openPDFs, state=DISABLED)
+pdfOpen.grid(row=25,column=9)
+
 
 tk.mainloop()
-
-
-
-
-
-
-
-        
-
-    
